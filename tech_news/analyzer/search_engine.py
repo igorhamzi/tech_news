bb3537fe0a28eb30138dba2dbc4f21791df29c93
IgorHamzi <igorwhamzi@gmail.com>
2022-11-17 20:15:27 -0300

    feat:create function get_tech_news

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,28 +2,32 @@ from tech_news.database import search_news
 import datetime
 
 
+def return_filters(filter):
+    return [(news["title"], news["url"]) for news in filter]
+
+
 def search_by_title(title):
-    list_news = search_news({"title":
-                            {"$regex": title, "$options": "i"}})
-    requested = [(news["title"], news["url"]) for news in list_news]
-    return requested
+    news_search_title = search_news({"title":
+                                    {"$regex": title, "$options": "i"}})
+    return return_filters(news_search_title)
 
 
 # Requisito 7
 def search_by_date(date):
     date = datetime.date.fromisoformat(date).strftime("%d/%m/%Y")
     try:
-        list_news = search_news({"timestamp":
-                                {"$eq": date}})
-        requested = [(news["title"], news["url"]) for news in list_news]
-        return requested
+        news_search_date = search_news({"timestamp":
+                                       {"$eq": date}})
+        return return_filters(news_search_date)
     except ValueError:
         raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    news_search_tag = search_news({"tags":
+                                  {"$regex": tag, "$options": "i"}})
+    return return_filters(news_search_tag)
 
 
 # Requisito 9
