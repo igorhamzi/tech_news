2a16db81c7c2362138ef5425a1ef5ce76530171b
IgorHamzi <igorwhamzi@gmail.com>
2022-11-17 21:18:15 -0300

    refactor: function search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -12,25 +12,22 @@ def search_by_title(title):
     return return_filters(news_search_title)
 
 
-# Requisito 7
 def search_by_date(date):
     date_formated = datetime.date.fromisoformat(date).strftime("%d/%m/%Y")
     try:
         news_search_date = search_news({"timestamp":
                                        {"$eq": date_formated}})
-        return return_filters(news_search_date)
+        return [(news["title"], news["url"]) for news in news_search_date]
     except ValueError:
         raise ValueError("Data inválida")
 
 
-# Requisito 8
 def search_by_tag(tag):
     news_search_tag = search_news({"tags":
                                   {"$regex": tag, "$options": "i"}})
     return return_filters(news_search_tag)
 
 
-# Requisito 9
 def search_by_category(category):
     news_search_category = search_news({"category":
                                        {"$regex": category, "$options": "i"}})
