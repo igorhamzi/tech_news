8f86ff8a168a56ca48be1544b9f3f65a0af84b35
IgorHamzi <igorwhamzi@gmail.com>
2022-11-17 22:03:41 -0300

    fix: top_5_news

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,26 @@
-# Requisito 12
+# import sys
+
+
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    ...
+    '''options = int(input(Selecione uma das opções a seguir:
+                         0 - Popular o banco com notícias;
+                         1 - Buscar notícias por título;
+                         2 - Buscar notícias por data;
+                         3 - Buscar notícias por tag;
+                         4 - Buscar notícias por categoria;
+                         5 - Listar top 5 notícias;
+                         6 - Listar top 5 categorias;
+                         7 - Sair.))
+    if options == 0:
+        input("Digite quantas notícias serão buscadas:")
+    elif options == 1:
+        input("Digite o título:")
+    elif options == 2:
+        input("Digite a data no formato aaaa-mm-dd:")
+    elif options == 3:
+        input("Digite a tag:")
+    elif options == 4:
+        input("Digite a categoria:")
+    else:
+        sys.stderr.write("Opção inválida")'''
